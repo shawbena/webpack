7e1514b36c5558113a64429916d19ad010bb8519
shaw <shawbena@outlook.com>
2017-05-27 17:30:41 +0800

    update

--- 002/webpack.config.js
+++ 002/webpack.config.js
@@ -3,22 +3,20 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: './entry.js',
     output: {
-        path: __dirname,    //??
+        path: __dirname, //??
         filename: 'bundle.js'
     },
     module: {
-        loaders:[
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                
+        loaders: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+
                 use: "css-loader"
-                })
-            }
-        ]
+            })
+        }]
     },
     plugins: [
         //给输出文件头部加注释信息
-         new ExtractTextPlugin("styles.css"),
+        new ExtractTextPlugin("styles.css"),
     ]
 };
