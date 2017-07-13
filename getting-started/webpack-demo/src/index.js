2c1a592935c7fde3ca5f1749084adbb96d74d61f
shawbena <shaw@shawbenadeMacBook-Pro.local>
2017-07-13 16:18:08 +0800

    update

--- getting-started/webpack-demo/src/index.js
+++
@@ -1,9 +0,0 @@
-function component(){
-	var element = document.createElement('div');
-	//locash
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-	return element;
-}
-
-document.body.appendChild(component());
